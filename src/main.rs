82dedfa82128024306adbccb0ae578e1ce3286c5
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2024-02-28 00:02:18 +0100

    fix: read from stdin if "-" is in the list of files or empty

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,34 @@ use std::thread;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
-    if args.is_empty() {
-        eprintln!("Usage: <program> <file1> <file2> ...");
-        std::process::exit(1);
-    }
-
     let (tx, rx) = mpsc::channel();
     let mut handles = Vec::new();
 
+    if args.is_empty() || args.contains(&"-".to_string()) {
+        let tx_clone = tx.clone();
+        let handle = thread::spawn(move || {
+            let stdin = io::stdin();
+            let reader = stdin.lock();
+            for line in reader.lines() {
+                let line = line.expect("error reading line from stdin");
+                tx_clone.send(line).expect("error sending line from stdin");
+            }
+        });
+        handles.push(handle);
+    }
+
     for path in args {
+        if path == "-" {
+            continue; // Skip "-" since we've already handled stdin.
+        }
+
         let tx_clone = tx.clone();
         let handle = thread::spawn(move || {
             let file = File::open(&path).expect("cannot open file");
             let reader = io::BufReader::new(file);
             for line in reader.lines() {
-                let line = line.expect("error reading line");
-                tx_clone.send(line).expect("error sending line");
+                let line = line.expect("error reading line from file");
+                tx_clone.send(line).expect("error sending line from file");
             }
         });
         handles.push(handle);
